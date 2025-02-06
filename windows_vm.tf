cf61f150f0d53d82f1baab79653e8f455fde7702
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-05 21:22:06 -0500

    Create windows_vm.tf

---
+++ windows_vm.tf
@@ -0,0 +1,108 @@
+
+
+# Variables
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "rg-benito"
+}
+
+variable "vm_name" {
+  default = "windows-vm"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "YourP@ssw0rd!"  # Remplacez par un mot de passe sécurisé
+}
+
+# Ressource de groupe de ressources
+resource "azurerm_resource_group" "rg_benito" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Ressource de réseau virtuel
+resource "azurerm_virtual_network" "vnet_benito" {
+  name                = "vnet-windows-vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_benito.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Sous-réseau pour la machine virtuelle
+resource "azurerm_subnet" "subnet_benito" {
+  name                 = "subnet-windows-vm"
+  resource_group_name  = azurerm_resource_group.rg_benito.name
+  virtual_network_name = azurerm_virtual_network.vnet_benito.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Adresse IP publique pour la machine virtuelle
+resource "azurerm_public_ip" "public_ip_benito" {
+  name                = "public-ip-windows-vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_benito.name
+  allocation_method   = "Dynamic"
+}
+
+# Interface réseau de la machine virtuelle
+resource "azurerm_network_interface" "nic_benito" {
+  name                = "nic-windows-vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_benito.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_benito.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip_benito.id
+  }
+}
+
+# Machine virtuelle Windows
+resource "azurerm_virtual_machine" "benito_vm" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg_benito.name
+  network_interface_ids = [azurerm_network_interface.nic_benito.id]
+  vm_size               = "Standard_B1ms"  # Changez cette valeur selon vos besoins
+
+  storage_os_disk {
+    name              = "${var.vm_name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed           = true
+    os_type           = "Windows"
+  }
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"  # Changez la version de Windows Server si nécessaire
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+    enable_automatic_upgrades = true
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip_benito.ip_address
+}
